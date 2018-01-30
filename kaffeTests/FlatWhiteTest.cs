0f512f4903b1a55c68d7786f833ead30cc2983d4
Gordon Mikkelsen <gordonmikkelsen87@gmail.com>
2018-01-30 13:52:28 +0100

    Test FlatWhite done

---
+++ kaffeTests/FlatWhiteTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using kaffe;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace kaffe.Tests
+{
+    [TestClass]
+    public class FlatWhiteTest
+    {
+        [TestMethod]
+        public void PrisTestFlatWhite()
+        {
+            // Arrange 
+            var flatWhite = new FlatWhite();
+            // Act
+            int pris = flatWhite.Pris();
+            // Assert
+            Assert.AreEqual(45, pris);
+        }
+
+        [TestMethod]
+        public void StyrkeTestFlatWhite()
+        {
+            // Arrange 
+            var flatWhite = new FlatWhite();
+            //Act
+            string styrke = flatWhite.Styrke();
+            // Assert
+            Assert.AreEqual("Mild", styrke);
+        }
+
+        [TestMethod]
+
+        public void MlMælk()
+        {
+            // Arrange 
+            var flatwhite = new FlatWhite();
+            //Act
+            int ml = flatwhite.MlMælk();
+            // Asset
+            Assert.AreEqual(160, ml);
+        }
+
+    }
+}
